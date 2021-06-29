9e4cd6f0a0f44e8b5b99d9b9b2eee0d9dceb5cf8
Kyle Hilton <kylehilton92@gmail.com>
2021-06-28 19:58:01 -0600

    Updates

---
+++ templates/__react_static_root__/src/pages/index.tsx.21b7415c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{63:function(e,n,o){"use strict";o.r(n);var t=o(0),a=o.n(t),c=o(662),r=o(17);n.default=function(){var e=Object(r.useRouteData)().content;return console.log(e),a.a.createElement(c.a.Markdown,{source:e})}}}]);
